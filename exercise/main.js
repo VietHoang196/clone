cc2e7cf86745534e72ac61811a17b44b276b37c4
VietHoang196 <viethoang9896@gmail.com>
2023-06-19 14:24:21 +0700

    update 19/6

--- exercise/main.js
+++ exercise/main.js
@@ -1,42 +1,40 @@
 // XỬ LÝ SỐ
-//Bài1
+//Ex 1
 function the_tich_hinh_cau(r) {
-
     const result = 4 / 3 * Math.PI * Math.pow(r, 3)
     console.log(result)
 }
 
 
-
-//bai 2
-function tong_cac_so_nam_giua(number_1, number_2) {
-    let bai_1_2 = 0
+//Ex 2
+function tong_cac_so_nam_giua(num_1, num_2) {
+    let total = 0
     let arr = []
-    if (number_1 < number_2) {
-        for (let i = number_1 + 1; i < number_2; i++) {
+    if (num_1 < num_2) {
+        for (let i = num_1 + 1; i < num_2; i++) {
             arr.push(i)
         }
         for (const item of arr) {
-            bai_1_2 += item
+            total += item
         }
-        console.log(bai_1_2)
+        console.log(total)
     }
-    if (number_1 > number_2) {
-        for (i = number_2 + 1; i < number_1; i++) {
+    if (num_1 > num_2) {
+        for (i = num_2 + 1; i < num_1; i++) {
             arr.push(i)
         }
         for (const item of arr) {
-            bai_1_2 += item
+            total += item
         }
-        console.log(bai_1_2)
+        console.log(total)
     }
 }
 
 
-//bai 3
+//Ex 3
 function tong_cac_uoc_so(number) {
     let arr = []
-    let bai_1_3 = 0
+    let total = 0
     for (let i = 1; i <= number; i++) {
         if (number % i === 0) {
             arr.push(i)
@@ -44,42 +42,39 @@ function tong_cac_uoc_so(number) {
     }
 
     for (const numb of arr) {
-        bai_1_3 += numb
+        total += numb
     }
-    console.log(bai_1_3)
+    console.log(total)
 }
 
 
 
-//bai 4
+//Ex 4
 function kiem_tra_snt(number) {
-    let bai_1_4
+    let test
     if (number <= 1) return false
     else {
         for (let i = 2; i < number; i++) {
             if (number % i === 0) {
-                bai_1_4 = false
+                test = false
                 break
             }
             else {
-                bai_1_4 = true
+                test = true
             }
         }
     }
-    console.log(bai_1_4)
+    console.log(test)
 }
 
 
 
-//bai 5
+//Ex 5
 function isNT(number) {
     for (let i = 2; i < number; i++) {
         if (number % i === 0) {
             return false
         }
-        // else{
-        //     return true
-        // }
     }
     return true
 }
@@ -96,7 +91,7 @@ function tong_cac_so_nguyen_to(number) {
 
 
 /*XỬ LÝ CHUỖI*/
-//bai 1
+//Ex 1
 let str = 'HELLO World'
 let toLower = str.toLowerCase()
 let arrChars = toLower.split('')
@@ -115,7 +110,7 @@ for (let i = 1; i < arrChars.length; i++) {
 newArr.join('')
 
 
-//bai 2
+//Ex 2
 function thay_doi_gach_ngang() {
     let str = 'HELLO World'
     let toLower = str.toLowerCase()
@@ -134,7 +129,7 @@ function thay_doi_gach_ngang() {
 }
 
 /*XỬ LÝ MẢNG*/
-//bai 1
+//Ex 1
 function loai_gia_tri_sai() {
     let arr_bai_1 = [1, 2, 3, undefined, null, '', false]
     let arr_result = arr_bai_1.filter(function (x) {
@@ -145,7 +140,7 @@ function loai_gia_tri_sai() {
 }
 
 
-//bai 2
+//Ex 2
 function tim_chuoi_dai_nhat() {
     let arr = ['aaa', 'bb', 'cc', 'd', '123']
     let max_length = 0
@@ -164,3 +159,4 @@ function tim_chuoi_dai_nhat() {
     console.log(result)
 }
 tim_chuoi_dai_nhat()
+
